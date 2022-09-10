3a6ec6efb1811625f3583dc680a1954ed48c78a6
Ibrahim-Mowafy <ibrahim.mowafy10@gmail.com>
2022-09-10 15:12:20 +0200

    Resolve conflicts in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
   content: [
     './screens/**/*.{js,jsx,ts,tsx}',
     './pages/**/*.{js,jsx,ts,tsx}',
     './components/**/*.{js,jsx,ts,tsx}',
   ],
-=======
-  content: ['./App.{js,jsx,ts,tsx}', './<custom-folder>/**/*.{js,jsx,ts,tsx}'],
->>>>>>> 8a7b72c588df6d817d7a92d8829f15f972bc6799
   theme: {
     extend: {},
   },
