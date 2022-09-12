0d53b31fb3a4570db530e420fca2b95d039d3f8d
Ibrahim-Mowafy <ibrahim.mowafy10@gmail.com>
2022-09-12 12:48:35 +0200

    Adding redux store

---
+++ store/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from '@reduxjs/toolkit';
+import basketReducer from './basketSlice';
+import restaurantReducer from './restaurantSlice';
+
+export const store = configureStore({
+  reducer: { basket: basketReducer, restaurant: restaurantReducer },
+});
